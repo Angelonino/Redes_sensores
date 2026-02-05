ad04d439582085ca52494829540587f8fc75a9ba
Angelo Niño Cantando <angelatoxx@outlook.com>
2026-02-05 20:33:23 +0100

    Update practica_1.ino

--- PRACTICAS/Practica_1/practica_1/practica_1.ino
+++ PRACTICAS/Practica_1/practica_1/practica_1.ino
@@ -1,3 +1,10 @@
+#define pin_ADC 4
+
+int V_ADC=0;
+float V_real=0.0;
+int V_int=0;
+char buffer[125];
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -5,5 +12,9 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  V_ADC=analogRead(pin_ADC);
+  V_real=(V_ADC*3.3)/4095; //precisión del ADC de 12bits
+  V_int=(V_ADC*3300)/4095;
+  sprintf(buffer, "Input Voltage: %d", V_int);
+  delay(1000);
 }
