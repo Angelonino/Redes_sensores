563accdf0619cba3d2fabb04086cf23b198846c5
Angelo Niño Cantando <angelatoxx@outlook.com>
2026-02-06 20:21:57 +0100

    Apartado 1
    Lectura del ADC y escritura en la consola del valor de tensión medida

--- PRACTICAS/Practica_1/practica_1/practica_1.ino
+++ PRACTICAS/Practica_1/practica_1/practica_1.ino
@@ -1,4 +1,4 @@
-#define pin_ADC 4
+#define pin_ADC A0
 
 int V_ADC=0;
 float V_real=0.0;
@@ -7,14 +7,19 @@ char buffer[125];
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600); //Baudrate
+  Serial.println("Comunicación serie iniciada");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   V_ADC=analogRead(pin_ADC);
-  V_real=(V_ADC*3.3)/4095; //precisión del ADC de 12bits
-  V_int=(V_ADC*3300)/4095;
+  //V_real=(V_ADC*3.3)/1023; //precisión del ADC de 10 bits
+  V_int=(V_ADC*3300)/1023;
+  V_real=(float)V_int/1000.0;
+  
   sprintf(buffer, "Input Voltage: %d", V_int);
+  Serial.println(buffer);
   delay(1000);
 }
+
